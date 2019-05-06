ae41ab831780345b2cb6edc64f26f04584704aaf
PabloDelBarrioArnanz <47177543+PabloDelBarrioArnanz@users.noreply.github.com>
2019-05-06 20:02:39 +0200

    fixed issue #2 parte cuenta

--- src/main/java/etsisi/ems/trabajo3/banco/Cuenta.java
+++ src/main/java/etsisi/ems/trabajo3/banco/Cuenta.java
@@ -15,17 +15,21 @@ public class Cuenta {
 		mTitular = titular;
 		mMovimientos = new Vector<Movimiento>();
 	}
-
-	public void ingresar(double x) throws Exception {
-		if (x <= 0)
-			throw new Exception("No se puede ingresar una cantidad negativa");
+	
+	public void aniadirMov(double x, String concepto) {
 		Movimiento m = new Movimiento();
-		m.setConcepto("Ingreso en efectivo");
+		m.setConcepto(concepto);
 		m.setImporte(x);
 		Date date = new Date();
 		LocalDate fecha = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
 		m.setFecha(fecha);
 		this.mMovimientos.addElement(m);
+	}
+	
+	public void ingresar(double x) throws Exception {
+		if (x <= 0)
+			throw new Exception("No se puede ingresar una cantidad negativa");
+		aniadirMov(x, "Ingresar efectivo");
 	}
 
 	public void retirar(double x) throws Exception {
@@ -33,26 +37,14 @@ public class Cuenta {
 			throw new Exception("No se puede retirar una cantidad negativa");
 		if (getSaldo() < x)
 			throw new Exception("Saldo insuficiente");
-		Movimiento m = new Movimiento();
-		m.setConcepto("Retirada de efectivo");
-		m.setImporte(-x);
-		Date date = new Date();
-		LocalDate fecha = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-		m.setFecha(fecha);
-		this.mMovimientos.addElement(m);
+		aniadirMov(x, "Retirada de efectivo");
 
 	}
 
 	public void ingresar(String concepto, double x) throws Exception {
 		if (x <= 0)
 			throw new Exception("No se puede ingresar una cantidad negativa");
-		Movimiento m = new Movimiento();
-		m.setConcepto(concepto);
-		m.setImporte(x);
-		Date date = new Date();
-		LocalDate fecha = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-		m.setFecha(fecha);
-		this.mMovimientos.addElement(m);
+		aniadirMov(x, concepto);
 	}
 
 	public void retirar(String concepto, double x) throws Exception {
@@ -60,13 +52,7 @@ public class Cuenta {
 			throw new Exception("No se puede retirar una cantidad negativa");
 		if (getSaldo() < x)
 			throw new Exception("Saldo insuficiente");
-		Movimiento m = new Movimiento();
-		m.setConcepto(concepto);
-		m.setImporte(-x);
-		Date date = new Date();
-		LocalDate fecha = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-		m.setFecha(fecha);
-		this.mMovimientos.addElement(m);
+		aniadirMov(x, concepto);
 	}
 
 	public double getSaldo() {
